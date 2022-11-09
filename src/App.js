9526127894cf510e03e0adc12bc9a2425ed60ee3
Joana <joanamdsantos1@gmail.com>
2022-11-09 17:43:41 -0300

    criação das páginas e das rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from './trivia.png';
 import './App.css';
+import Play from './pages/Play';
+import Login from './pages/Login';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEzsZ</p>
+        <p>SUA VEZ</p>
       </header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route path="/play" component={ Play } />
+          <Route path="/feedback" component={ Feedback } />
+        </Switch>
+      </main>
     </div>
   );
 }
--- src/redux/reducer/player.js
+++ src/redux/reducer/player.js
@@ -1,8 +1,6 @@
 import { SAVEUSER } from '../actions';
 
 const INITIAL_STATE = {
-  player: xxx,
-
 };
 
 const player = (state = INITIAL_STATE, { type, payload }) => {
