22562470ba74a4101a5eb66e3739a37ccbafb2ef
Joana <joanamdsantos1@gmail.com>
2022-11-18 20:07:53 -0300

    criando a pasta de mocks e o mockQuiz

---
+++ src/tests/mocks/mockQuiz.js
@@ -0,0 +1,64 @@
+const mockQuiz = [
+    {
+    "category": "General Knowledge",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "What is a &quot;dakimakura&quot;?",
+    "correct_answer": "A body pillow",
+    "incorrect_answers": [
+        "A Chinese meal, essentially composed of fish",
+        "A yoga posture",
+        "A word used to describe two people who truly love each other"
+    ]
+},
+{
+    "category": "Geography",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "Which German city is located on the River Isar?",
+    "correct_answer": "Munich",
+    "incorrect_answers": [
+        "Berlin",
+        "Hamburg",
+        "Dortmund"
+    ]
+},
+{
+    "category": "Celebrities",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "If he was still alive, in what year would Elvis Presley celebrate his 100th birthday?",
+    "correct_answer": "2035",
+    "incorrect_answers": [
+        "2030",
+        "2040",
+        "2045"
+    ]
+},
+{
+    "category": "Entertainment: Video Games",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "In Hitman: Blood Money, what is the name of the target in the mission &quot;Death of a Showman&quot;?",
+    "correct_answer": "Joseph Clarence",
+    "incorrect_answers": [
+        "The Swing King",
+        "Maynard John",
+        "Manuel Delgado"
+    ]
+},
+{
+    "category": "Entertainment: Television",
+    "type": "multiple",
+    "difficulty": "medium",
+    "question": "In which year did the British television series &quot;The Bill&quot; end?",
+    "correct_answer": "2010",
+    "incorrect_answers": [
+        "2001",
+        "2007",
+        "2012"
+    ]
+}
+]
+
+export default mockQuiz;
