0b41585ef08d9875246e01c76ec6edc9c3cb26b4
Joana <joanamdsantos1@gmail.com>
2022-11-19 12:39:25 -0300

    21. Desenvolva testes para atingir 90% de cobertura da tela de Jogo

--- src/tests/game.test.js
+++ src/tests/game.test.js
@@ -1,8 +1,11 @@
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
+import mockInvalidToken from './mocks/mockInvalidToken';
 import mockQuiz from './mocks/mockQuiz';
+import mockToken from './mocks/mockToken';
 
 describe('Testa a tela de jogo', () => {
   test('Testa se a tela é renderizada na rota correta', () => {
@@ -24,17 +27,77 @@ describe('Testa a tela de jogo', () => {
     })
   })
 
-  // test('Testa se renderiza as alternativas', () => {
-  //   renderWithRouterAndRedux(<App />, {}, '/play');
+  test('Testa se redireciona a pagina inicial se o token é inválido', () => {
+    act(() => {
+      jest.spyOn(global, 'fetch');
+      global.fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValueOnce(mockInvalidToken).mockResolvedValue(mockQuiz),
+      });
+    })
     
-  //   global.fetch = jest.fn(() => Promise.resolve({
-  //     json: () => Promise.resolve(mockQuiz),
-  //   }));
+    const { history } = renderWithRouterAndRedux(<App />, {}, '/play');
+    
 
-  //   waitFor(() => {
-  //     const correctAnswer = screen.findByTestId('correct-answer');
+    waitFor(() => {
+      const { pathname } = history.location;
 
-  //     expect(correctAnswer).toBeInTheDocument();
-  //   })
-  // })
+      expect(pathname).toBe('/');
+    })
+  })
+
+  test('Testa se renderiza as alternativas', () => {
+    act(() => {
+      jest.spyOn(global, 'fetch');
+      global.fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValueOnce(mockToken).mockResolvedValue(mockQuiz),
+      });
+    })
+
+    renderWithRouterAndRedux(<App />, {}, '/play');
+    
+
+    waitFor(() => {
+      const alternatives = screen.findAllByRole('button');
+
+      expect(alternatives).toHaveLength(4);
+    })
+  })
+
+  test('Testa se não renderiza o botão next quando as perguntas são renderizadas', () => {
+    act(() => {
+      jest.spyOn(global, 'fetch');
+      global.fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValueOnce(mockToken).mockResolvedValue(mockQuiz),
+      });
+    })
+
+    renderWithRouterAndRedux(<App />, {}, '/play');
+    
+
+    waitFor(() => {
+      const nextButton = screen.queryByTestId('btn-next');
+
+      expect(nextButton).toBeNull();
+    })
+  })
+
+  test('Testa se o botão next é renderizado após responder a pergunta', () => {
+    act(() => {
+      jest.spyOn(global, 'fetch');
+      global.fetch.mockResolvedValue({
+        json: jest.fn().mockResolvedValueOnce(mockToken).mockResolvedValue(mockQuiz),
+      });
+    })
+
+    renderWithRouterAndRedux(<App />, {}, '/play');
+    
+
+    waitFor(() => {
+      const correctAnswer = screen.getByTestId('correct-answer');
+      userEvent.click(correctAnswer)
+      const nextButton = screen.queryByTestId('btn-next');
+
+      expect(nextButton).toBeInTheDocument();
+    })
+  })
 })
